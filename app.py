c46713e786dfec7e9277269d83a7d92cc417307f
Aron William Bechen <williambechen@gmail.com>
2024-10-24 14:46:01 +0200

    1

--- app.py
+++ app.py
@@ -151,8 +151,9 @@ if not app.debug:
 if __name__ == "__main__":
     app.run(debug=True)
 
-# Or specify port manually:
 """
+# Or specify port manually:
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
